91125b39590356b940010b59ecb7f9d245bfb75b
LironeA <322004@ukr.net>
2023-11-19 20:23:15 +0200

    Lab3

--- Lab3/Core/Tokenizer.cs
+++ Lab3/Core/Tokenizer.cs
@@ -21,6 +21,11 @@ public static class Tokenizer
     private static string reservedWordsRegex =
         @"\b(?<!#)(abstract|as|base|bool|break|byte|case|catch|char|checked|class|const|continue|decimal|default|delegate|do|double|else|enum|event|explicit|extern|false|finally|fixed|float|for|foreach|goto|if|implicit|int|interface|internal|is|lock|long|namespace|new|null|object|operator|out|override|params|private|protected|public|readonly|ref|return|sbyte|sealed|short|sizeof|stackalloc|static|string|struct|switch|this|throw|true|try|typeof|uint|ulong|unchecked|unsafe|ushort|using|virtual|void|volatile|while)\b";
 
+    private static string identifierRegex =
+        @"[a-zA-Z_][a-zA-Z0-9_]*";
+    
+    private  static string numberRegex =
+        @"[0-9]+(\.[0-9]+)?";
     
     static Tokenizer() {}
 
@@ -33,15 +38,6 @@ public static class Tokenizer
         input = input.Substring(1, input.Length - 2);
         
         
-        var delimeters = Regex.Matches(input, delimetersRegex, RegexOptions.Multiline);
-        
-        foreach (Match match in delimeters)
-        {
-            var token = new Token();
-            token.Value = match.Value;
-            token.Type = TokenType.Delimiter;
-            inputFile.Tokens.Add(token);
-        }
         
         var comments = Regex.Matches(input, commentsRegex, RegexOptions.Multiline);
         
@@ -73,6 +69,17 @@ public static class Tokenizer
             inputFile.Tokens.Add(token);
         }
         
+        var delimeters = Regex.Matches(input, delimetersRegex, RegexOptions.Multiline);
+        
+        foreach (Match match in delimeters)
+        {
+            var token = new Token();
+            token.Value = match.Value;
+            token.Type = TokenType.Delimiter;
+            if(inputFile.Tokens.Find(x => x.Value == token.Value) == null)
+                inputFile.Tokens.Add(token);
+        }
+        
         var reservedWords = Regex.Matches(input, reservedWordsRegex, RegexOptions.Multiline);
         
         foreach (Match match in reservedWords)
@@ -81,6 +88,27 @@ public static class Tokenizer
             var token = new Token();
             token.Value = match.Value;
             token.Type = TokenType.ReservedWord;
+            inputFile.Tokens.Add(token);
+        }
+        
+        var identifiers = Regex.Matches(input, identifierRegex, RegexOptions.Multiline);
+        
+        foreach (Match match in identifiers)
+        {
+            var token = new Token();
+            token.Value = match.Value;
+            token.Type = TokenType.Identifier;
+            if(inputFile.Tokens.Find(x => x.Value == token.Value) == null)
+                inputFile.Tokens.Add(token);
+        }
+        
+        var numbers = Regex.Matches(input, numberRegex, RegexOptions.Multiline);
+        
+        foreach (Match match in numbers)
+        {
+            var token = new Token();
+            token.Value = match.Value;
+            token.Type = TokenType.Number;
             inputFile.Tokens.Add(token);
         }
         
