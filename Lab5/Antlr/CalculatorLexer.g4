1d57f04c372b4fc00ddcaa7831264eb71e1bafbe
LironeA <322004@ukr.net>
2023-12-03 14:26:15 +0200

    Lab5

---
+++ Lab5/Antlr/CalculatorLexer.g4
@@ -0,0 +1,14 @@
+﻿lexer grammar CalculatorLexer;
+
+PLUS: '+';
+MINUS: '-';
+STAR: '*';
+SLASH: '/';
+
+PAREN_OPEN: '(';
+PAREN_CLOSE: ')';
+
+INT: ('-')? [0-9]+;
+DOUBLE: ('-')? [0-9]+ '.' [0-9]+;
+
+WS: [ \t\n\r] -> skip;
---
+++ Lab5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Lab5.Antlr;
+using Antlr4.Runtime;
+
+namespace Lab5.Calculator
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            const string exp = "(2*2) + 15/2";
+            var inputStream = new AntlrInputStream(exp);
+            var lexer = new CalculatorLexer(inputStream);
+
+            var commonTokenStream = new CommonTokenStream(lexer);
+            var parser = new CalculatorParser(commonTokenStream);
+
+            var visitor = new CalculatorVisitor();
+
+            var resut = visitor.Visit(parser.expr());
+
+            Console.WriteLine(resut);
+        }
+    }
+}
